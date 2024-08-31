0b42a07ea4b77f3c2727d19b12cbbc8c29901721
llanosmjla <llanos.mendoza.joseluisalberto@usfx.bo>
2024-08-31 02:14:43 -0400

    I was doing changes

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,11 @@ const nextConfig = {
     images: {
         domains: ['media.gq.com.mx', 'image.tmdb.org', 'ui-avatars.com', 'api.themoviedb.org'],
     },
-    swcMinify: false, // Desactiva la minificación con SWC
+    /*swcMinify: false, // Desactiva la minificación con SWC
     webpack: (config) => {
       config.optimization.minimize = false; // Desactiva la minificación con Terser
       return config;
-    },
+    },*/
 };
 
 export default nextConfig;
